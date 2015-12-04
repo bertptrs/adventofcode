d2e12ccd8645b9a413f524a323453a2e0dd4f4fd
Bert Peters <bert.ljpeters@gmail.com>
2015-12-04 22:28:03 +0100

    Day 3 and 4 solutions.

---
+++ day3.py
@@ -0,0 +1,28 @@
+import fileinput
+
+def move(pos, c):
+    if c == '<':
+        pos = (pos[0] + 1, pos[1])
+    elif c == '>':
+        pos = (pos[0] - 1, pos[1])
+    elif c == '^':
+        pos = (pos[0], pos[1] + 1)
+    elif c == 'v':
+        pos = (pos[0], pos[1] - 1)
+
+    return pos
+
+pos = (0, 0)
+pos2 = pos
+places = set([pos])
+
+for line in fileinput.input():
+    for idx,c in enumerate(line):
+        if idx % 2 == 0:
+            pos = move(pos, c)
+            places.add(pos)
+        else:
+            pos2 = move(pos2, c)
+            places.add(pos2)
+
+print "Houses:", len(places)
