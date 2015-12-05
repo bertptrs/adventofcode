16a0640a07008cabfe94eb01520bb5b3e1c6ce2d
Bert Peters <bert.ljpeters@gmail.com>
2015-12-05 20:34:30 +0100

    Add python implementation of day1, after the fact.

---
+++ day1.py
@@ -0,0 +1,17 @@
+import fileinput
+
+floor = 0
+seenBasement = False
+
+for line in fileinput.input():
+    for idx, c in enumerate(line.strip()):
+        if c == '(':
+            floor += 1
+        else:
+            floor -= 1
+
+        if not seenBasement and floor == -1:
+            seenBasement = True
+            print "Basement reached at", idx + 1
+
+print "Finally arrived at", floor
