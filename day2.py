959aa387857c6cfabce3a17274f51235f423eddb
Bert Peters <bert.ljpeters@gmail.com>
2015-12-05 20:16:42 +0100

    Add solution day 2.

---
+++ day2.py
@@ -0,0 +1,19 @@
+import fileinput
+from operator import mul
+
+totalArea = 0
+totalRibbon = 0
+for line in fileinput.input():
+    parts = [int(i) for i in line.split('x')]
+    parts.sort()
+
+    sides = [parts[0] * parts[1], parts[0] * parts[2], parts[1] * parts[2]]
+    totalArea += 2 * sum(sides)
+
+    totalArea += min(sides)
+    totalRibbon += 2 * (parts[0] + parts[1])
+    totalRibbon += reduce(mul, parts, 1)
+
+print totalArea, "paper"
+print totalRibbon, "ribbon"
+
