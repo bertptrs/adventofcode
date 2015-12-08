8329f0658265c1c09b93ae3873119daa91bfed55
Bert Peters <bert.ljpeters@gmail.com>
2015-12-08 13:39:01 +0100

    Add solutions day 8.

---
+++ day8.py
@@ -0,0 +1,30 @@
+import fileinput
+import re
+
+totalLess = 0
+totalMore = 0
+
+for line in fileinput.input():
+    original = len(line)
+    diff = 2
+    plus = 2
+    escaped = False
+    for c in line:
+        if c == "\\" or c == "\"":
+            plus += 1
+
+        if c == "\\" and not escaped:
+            escaped = True
+            diff += 1
+            continue
+
+        if escaped and c == "x":
+            diff += 2
+
+        escaped = False
+
+    totalLess += diff
+    totalMore += plus
+
+print totalLess, "less"
+print totalMore, "more"
