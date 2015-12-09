752dcfac9b6a74a3548e784ad84b315b1bd9ea35
Bert Peters <bert.ljpeters@gmail.com>
2015-12-09 11:40:23 +0100

    Run day 4 with an input file now.

--- day-04/solution.py
+++ day-04/solution.py
@@ -1,15 +1,29 @@
 import sys
 import md5
+import fileinput
 
-word = sys.argv[1]
-number = 0
+def ok(digest, zeroes):
+    for c in digest[0:zeroes]:
+        if c != "0":
+            return False
 
-while True:
-    digester = md5.new(word)
-    digester.update(str(number))
+    return True
 
-    if digester.hexdigest()[0:6] == "000000":
-        print word, number
-        break
+def solve(word, zeroes):
+    number = 0
+    
+    while True:
+        digester = md5.new(word)
+        digester.update(str(number))
+    
+        if ok(digester.hexdigest(), zeroes):
+            print word, number
+            break
+    
+        number = number + 1
 
-    number = number + 1
+for line in fileinput.input():
+    word, zeroes = line.split("\t")
+    zeroes = int(zeroes.strip())
+
+    solve(word, zeroes)
