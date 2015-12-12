3b13925a0ebce08391509403ef0fb45ce3dd8c00
Bert Peters <bert.ljpeters@gmail.com>
2015-12-12 10:47:59 +0100

    Solutions for day 12.

---
+++ day-12/solution.py
@@ -0,0 +1,23 @@
+import fileinput
+import re
+import json
+
+def totals(obj):
+    if isinstance(obj, int):
+        return obj
+    if isinstance(obj, list):
+        return sum(totals(i) for i in obj)
+    if not isinstance(obj, dict) or "red" in obj.values():
+        return 0
+
+    return sum(totals(i) for i in obj.values())
+
+fileData = ''.join(line for line in fileinput.input())
+
+# Solve the first part by regex, no parsing needed.
+pattern = re.compile(r"-?\d+")
+total = sum(int(match) for match in pattern.findall(fileData))
+
+data = json.loads(fileData)
+
+print total, totals(data)
