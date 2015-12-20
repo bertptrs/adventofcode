c07780fcd3e7a3a0e3da8d56e6cb5adea2b291d2
Bert Peters <bert.ljpeters@gmail.com>
2015-12-20 23:07:26 +0100

    Solution for day 20.

---
+++ day-20/solution.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+def findFirst(data, target):
+    for idx, value in enumerate(data):
+        if value >= target:
+            return idx
+
+    return None
+
+target = 34000000
+
+# Target is achieved at itself/10, so reasonable upper bound.
+upperbound = target // 10
+
+# Use a varation of Erathostenes' sieve to compute the results
+sieve1 = [10] * (upperbound + 1)
+sieve2 = [10] * (upperbound + 1)
+
+for x in range(1, upperbound):
+    for y in range(x, upperbound, x):
+        sieve1[y] += 10 * x
+
+    for y in range(x, min(50 * x, upperbound) + 1, x):
+        sieve2[y] += 11 * x
+
+print("House", findFirst(sieve1, target))
+print("House", findFirst(sieve2, target))
