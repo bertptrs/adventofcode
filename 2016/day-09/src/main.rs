08373adbf53384e127a2043dd8041ce980fb6c3d
Bert Peters <bert.ljpeters@gmail.com>
2016-12-10 00:21:55 +0100

    Solution for day 9 part 2.

--- 2016/day-09/src/main.rs
+++ 2016/day-09/src/main.rs
@@ -1,9 +1,47 @@
+#[macro_use] extern crate lazy_static;
 extern crate regex;
 
 use regex::Regex;
 use std::env;
 use std::io::prelude::*;
 use std::fs::File;
+
+fn decoded_len(content: &str, recurse: bool) -> i64
+{
+    lazy_static!{
+        static ref R: Regex = Regex::new(r"\((\d+)x(\d+)\)").expect("Failed to compile regex");
+    }
+
+    let mut len = 0;
+    let mut pos = 0;
+
+    loop {
+        let todo = &content[pos..];
+        match R.captures(todo) {
+            Some(captures) => {
+                let start = todo.find(&captures[0]).unwrap();
+                pos += start;
+                len += start as i64;
+
+                let (length, amount) = (captures[1].parse::<i64>().unwrap(), captures[2].parse::<i64>().unwrap());
+
+                let part = &todo[start + captures[0].len()..start + captures[0].len() + length as usize];
+
+                let part_length = if recurse { decoded_len(part, true) } else { length };
+
+                pos += captures[0].len() as usize + length as usize;
+                len += part_length * amount;
+            },
+            None => {
+                // No more markers remaining, just append the rest
+                len += content.len() as i64 - pos as i64;
+                break;
+            },
+        }
+    }
+
+    return len;
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,28 +52,7 @@ fn main() {
 
     let content = data.trim();
 
-    let r = Regex::new(r"\((\d+)x(\d+)\)").expect("Failed to compile regex");
-
-    let mut len = 0;
-    let mut pos = 0;
-
-    loop {
-        let todo = &content[pos..];
-        match r.captures(todo) {
-            Some(captures) => {
-                let (start, _) = r.find(todo).unwrap();
-                let (length, amount) = (captures[1].parse::<i64>().unwrap(), captures[2].parse::<i64>().unwrap());
-                pos += start + captures[0].len() as usize + length as usize;
-                len += start as i64 + length as i64 * amount;
-            },
-            None => {
-                // No more markers remaining, just append the rest
-                len += content.len() as i64 - pos as i64;
-                break;
-            },
-        }
-    }
-
-    println!("Decoded text is {} long", len);
+    println!("Decoded v1 is {} long", decoded_len(content, false));
+    println!("Decoded v2 is {} long", decoded_len(content, true));
 
 }
