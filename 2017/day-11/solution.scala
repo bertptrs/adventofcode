8ea4e3afbb14035102f6b97b72620c1ad3bf474b
Bert Peters <bert@bertptrs.nl>
2017-12-11 11:52:48 +0100

    Implement day 11 in scala.

---
+++ 2017/day-11/solution.scala
@@ -0,0 +1,39 @@
+import scala.io.StdIn
+
+object solution {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+
+    val input = StdIn.readLine()
+    val dirs = input.trim.split(",")
+
+    follow(dirs)
+  }
+
+  def follow(dirs: Array[String]): Unit = {
+    var x = 0
+    var y = 0
+
+    var maxDist = 0
+
+    for (dir <- dirs) {
+      dir match {
+        case "s" => y -= 1
+        case "n" => y += 1
+        case "nw" => x -= 1
+        case "se" => x += 1
+        case "ne" => y += 1; x += 1
+        case "sw" => y -= 1; x -= 1
+      }
+
+      maxDist = Math.max(maxDist, dist(x, y))
+    }
+
+    println(dist(x, y))
+    println(maxDist)
+  }
+
+  def dist(x: Int, y: Int): Int = {
+    return Math.max(x, y);
+  }
+}
