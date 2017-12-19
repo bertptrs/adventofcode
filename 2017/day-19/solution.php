e89ef03c07a225bbc3d8aca82eaddbd1bb263420
Bert Peters <bert@bertptrs.nl>
2017-12-19 10:19:37 +0100

    Even simpler bounds check.

--- 2017/day-19/solution.php
+++ 2017/day-19/solution.php
@@ -11,16 +11,16 @@ $dir = [0, 1];
 $path = '';
 $steps = 0;
 
-while ($maze[$y][$x] != ' ') {
+while (($maze[$y][$x] ?? ' ') != ' ') {
     if (ctype_alpha($maze[$y][$x])) {
         $path .= $maze[$y][$x];
     } elseif ($maze[$y][$x] == '+') {
         if ($dir[0] == 0) {
-            $dir[0] = ($x == 0 || $maze[$y][$x - 1] == ' ') ? 1 : -1;
+            $dir[0] = ($maze[$y][$x + 1] ?? ' ') == ' ' ? -1 : 1;
             $dir[1] = 0;
         } else {
             $dir[0] = 0;
-            $dir[1] = ($y == 0 || $maze[$y - 1][$x] == ' ') ? 1 : -1;
+            $dir[1] = ($maze[$y + 1][$x] ?? ' ') == ' ' ? -1 : 1;
         }
     }
 
