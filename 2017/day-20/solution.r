46a6d6e861ae908687f52f1c96fa198e95b59a12
Bert Peters <bert@bertptrs.nl>
2017-12-20 14:18:32 +0100

    Add implementation for part 2.

--- 2017/day-20/solution.r
+++ 2017/day-20/solution.r
@@ -14,4 +14,16 @@ speeds <- rowSums(abs(input[,4:6]))
 perm <- order(accs, speeds)
 print(perm[1] - 1)
 
+for (i in 1:100) {
+	# Update positions, speeds
+	for (coord in 1:3) {
+		input[,coord+3] <- input[,coord+3] + input[,coord+6]
+		input[,coord] <- input[,coord] + input[,coord+3]
+	}
 
+	dups = !(duplicated(input[,1:3]) | duplicated(input[,1:3], fromLast=TRUE))
+
+	input = input[dups,]
+}
+
+print(nrow(input))
