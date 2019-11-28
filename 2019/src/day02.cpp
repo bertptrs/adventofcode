314184badc8d9ba78b93a4bc87dbacaaf0aaa4e5
Bert Peters <bert@bertptrs.nl>
2019-11-28 08:00:46 +0100

    Prepare all days skeletons.

---
+++ 2019/src/day02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "days.hpp"
+
+void aoc2019::day02_part1(std::istream &input, std::ostream &output) {
+	output << "Not implemented\n";
+}
+
+void aoc2019::day02_part2(std::istream &input, std::ostream &output) {
+	output << "Not implemented\n";
+}
