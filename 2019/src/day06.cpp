f9caa8d1a9651c12046e19e7ba85eef4d73ab65f
Bert Peters <bert@bertptrs.nl>
2019-12-06 07:26:46 +0100

    Partial day 06.

--- 2019/src/day06.cpp
+++ 2019/src/day06.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <regex>
 #include "days.hpp"
 
 void aoc2019::day06_part1(std::istream &input, std::ostream &output) {
+    std::string buffer;
+    std::regex regex("^([A-Z0-9]+)\\)([A-Z0-9]+)$");
+
+    while (std::getline(input, buffer)) {
+        std::smatch match_results;
+
+        if (!std::regex_match(buffer, match_results, regex)) {
+            std::string error = "Invalid line: ";
+            error += buffer;
+            throw std::domain_error(error);
+        }
+    }
 	output << "Not implemented\n";
 }
 
