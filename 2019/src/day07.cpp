ec081adba7cd8d6bead186a93b4f3191fb79e017
Bert Peters <bert@bertptrs.nl>
2019-12-09 18:24:17 +0100

    Implement day 9.

--- 2019/src/day07.cpp
+++ 2019/src/day07.cpp
@@ -5,127 +5,7 @@
 #include "utils.hpp"
 
 namespace {
-    class IntCodeComputer {
-    public:
-        explicit IntCodeComputer(std::vector<int> program, std::deque<int> initial_inputs) :
-                program{std::move(program)}, inputs{std::move(initial_inputs)} {
-        }
-
-        void run() {
-            while (ip < program.size()) {
-                switch (program[ip] % 100) {
-                    case 1:
-                        program[program[ip + 3]] = interpret_value(1) + interpret_value(2);
-                        ip += 4;
-                        break;
-
-                    case 2:
-                        program[program[ip + 3]] = interpret_value(1) * interpret_value(2);
-                        ip += 4;
-                        break;
-
-                    case 3:
-                        if (inputs.empty()) {
-                            return;
-                        }
-
-                        program[program[ip + 1]] = inputs.front();
-                        inputs.pop_front();
-                        ip += 2;
-                        break;
-
-                    case 4:
-                        outputSink->sendInput(interpret_value(1));
-                        ip += 2;
-                        break;
-
-                    case 5: // Jump if non-zero
-                        if (interpret_value(1)) {
-                            ip = interpret_value(2);
-                        } else {
-                            ip += 3;
-                        }
-                        break;
-
-                    case 6: // Jump if zero
-                        if (!interpret_value(1)) {
-                            ip = interpret_value(2);
-                        } else {
-                            ip += 3;
-                        }
-                        break;
-
-                    case 7: // equality
-                        program[program[ip + 3]] = interpret_value(1) < interpret_value(2);
-                        ip += 4;
-                        break;
-
-                    case 8: // less than
-                        program[program[ip + 3]] = interpret_value(1) == interpret_value(2) ? 1 : 0;
-                        ip += 4;
-                        break;
-
-                    case 99:
-                        halted = true;
-                        return;
-
-                    default:
-                        char buffer[30];
-                        std::snprintf(buffer, sizeof(buffer), "Invalid opcode: %d", program[ip]);
-
-                        throw std::domain_error(buffer);
-                }
-            }
-        }
-
-        void sendInput(int input) {
-            inputs.push_back(input);
-        }
-
-        void connectOutput(IntCodeComputer &computer) {
-            outputSink = &computer;
-        }
-
-        [[nodiscard]] bool isTerminated() const {
-            return halted;
-        }
-
-        [[nodiscard]] const std::deque<int> &currentInputs() const {
-            return inputs;
-        }
-
-    private:
-        std::vector<int> program;
-        std::deque<int> inputs = {};
-        IntCodeComputer *outputSink = nullptr;
-        int ip = 0;
-        bool halted = false;
-
-        [[nodiscard]] int interpret_value(int pos) const {
-            bool immediate;
-            switch (pos) {
-                case 1:
-                    immediate = program[ip] / 100 % 10;
-                    break;
-                case 2:
-                    immediate = program[ip] / 1000 % 10;
-                    break;
-
-                case 3:
-                    immediate = program[ip] / 10000 % 10;
-                    break;
-
-                default:
-                    throw std::out_of_range("Invalid position");
-            }
-
-            if (immediate) {
-                return program[ip + pos];
-            } else {
-                return program[program[ip + pos]];
-            }
-        }
-    };
+    using aoc2019::IntCodeComputer;
 
     int simulate(const std::vector<int> &program, const std::array<int, 5> &phases) {
         int state = 0;
@@ -138,10 +18,10 @@ namespace {
         return state;
     }
 
-    int simulate2(const std::vector<int> &program, const std::array<int, 5> &phases) {
+    int simulate2(const std::vector<std::int64_t> &program, const std::array<int, 5> &phases) {
         std::vector<IntCodeComputer> computers;
         for (int phase : phases) {
-            computers.emplace_back(program, std::deque<int>{phase});
+            computers.emplace_back(program, std::deque<int64_t>{phase});
         }
 
         for (int i = 0; i < computers.size(); ++i) {
@@ -174,7 +54,7 @@ void aoc2019::day07_part1(std::istream &input, std::ostream &output) {
 }
 
 void aoc2019::day07_part2(std::istream &input, std::ostream &output) {
-    const auto program = aoc2019::read_intcode(input);
+    const auto program = aoc2019::IntCodeComputer::read_intcode(input);
     std::array<int, 5> phases{5, 6, 7, 8, 9};
 
     int best = 0;
--- 2019/src/day09.cpp
+++ 2019/src/day09.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <deque>
 #include "days.hpp"
+#include "utils.hpp"
 
 void aoc2019::day09_part1(std::istream &input, std::ostream &output) {
-	output << "Not implemented\n";
+    auto program = IntCodeComputer::read_intcode(input);
+    std::deque<std::int64_t> outputs;
+
+    IntCodeComputer computer(std::move(program), { 1 });
+    computer.connectOutput(outputs);
+
+    computer.run();
+
+    if (outputs.size() != 1) {
+        std::cerr << "Error: " << outputs.size() << std::endl;
+        for (auto c : outputs) {
+            std::cerr << c << std::endl;
+        }
+    } else {
+        output << outputs.front() << std::endl;
+    }
 }
 
 void aoc2019::day09_part2(std::istream &input, std::ostream &output) {
-	output << "Not implemented\n";
+    auto program = IntCodeComputer::read_intcode(input);
+    std::deque<std::int64_t> outputs;
+
+    IntCodeComputer computer(std::move(program), { 2 });
+    computer.connectOutput(outputs);
+
+    computer.run();
+
+    output << outputs.front() << std::endl;
 }
