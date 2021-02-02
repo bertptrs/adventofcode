beae045f5518afaf6e71e822fddbdc1840dbcb4e
Bert Peters <bert@bertptrs.nl>
2021-02-02 19:57:24 +0100

    Implement 2019 day 16 part 2

--- 2019/aoc2019/day16.py
+++ 2019/aoc2019/day16.py
@@ -27,7 +27,31 @@ def simulate(numbers: List[int]) -> str:
     return ''.join(str(s) for s in numbers[:8])
 
 
+def simulate2(numbers: List[int]) -> str:
+    numbers = numpy.tile(numpy.array(numbers, dtype=numpy.int), 10000)
+    starting_index = 0
+
+    for n in numbers[:7]:
+        starting_index *= 10
+        starting_index += n
+
+    assert starting_index > len(numbers) / 2
+
+    numbers = numbers[starting_index:]
+
+    for _ in range(100):
+        numbers = numpy.abs(numpy.flip(numpy.cumsum(numpy.flip(numbers)))) % 10
+
+    return ''.join(str(s) for s in numbers[:8])
+
+
 def part1(data: TextIO) -> str:
     numbers = read_input(data)
 
     return simulate(numbers)
+
+
+def part2(data: TextIO) -> str:
+    numbers = read_input(data)
+
+    return simulate2(numbers)
