8ac446dc8f2f2a3bf410b6d2379fbaf8c722df6d
Bert Peters <bert@bertptrs.nl>
2021-11-20 11:57:32 +0100

    Initial 2021 runner

---
+++ 2021/src/day01.rs
@@ -0,0 +1,10 @@
+use crate::Solution;
+
+#[derive(Default)]
+pub struct Day01;
+
+impl Solution for Day01 {
+    fn part1(&mut self, _input: &mut dyn std::io::Read) -> String {
+        todo!()
+    }
+}
