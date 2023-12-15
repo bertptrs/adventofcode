e65f98c9c6e730e9a7b17bf1fddf5c9649cc27d1
Bert Peters <bert@bertptrs.nl>
2023-12-15 08:05:10 +0100

    Implement 2023 day 15 part 1

--- 2023/src/day15.rs
+++ 2023/src/day15.rs
@@ -1,7 +1,46 @@
-pub fn part1(_input: &[u8]) -> anyhow::Result<String> {
-    anyhow::bail!("Not implemented")
+fn trim(input: &[u8]) -> &[u8] {
+    let whitespace = input
+        .iter()
+        .rev()
+        .take_while(|c| c.is_ascii_whitespace())
+        .count();
+
+    &input[..(input.len() - whitespace)]
+}
+
+fn hash(input: &[u8]) -> u32 {
+    input
+        .iter()
+        .fold(0, |cur, &c| ((cur + u32::from(c)) * 17) % 256)
+}
+
+pub fn part1(input: &[u8]) -> anyhow::Result<String> {
+    let input = trim(input);
+
+    Ok(input
+        .split(|&c| c == b',')
+        .map(hash)
+        .sum::<u32>()
+        .to_string())
 }
 
 pub fn part2(_input: &[u8]) -> anyhow::Result<String> {
     anyhow::bail!("Not implemented")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE: &[u8] = include_bytes!("samples/15.txt");
+
+    #[test]
+    fn sample_hash() {
+        assert_eq!(hash(b"HASH"), 52);
+    }
+
+    #[test]
+    fn sample_part1() {
+        assert_eq!("1320", part1(SAMPLE).unwrap());
+    }
 }
