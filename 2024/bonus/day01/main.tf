73f886359bf386fb0f1555b83ae568c74319e4f2
Bert Peters <bert@bertptrs.nl>
2024-12-01 16:51:45 +0100

    Implement day 1 part 1 in Terraform

---
+++ 2024/bonus/day01/main.tf
@@ -0,0 +1,20 @@
+variable "input" {
+  type = string
+}
+
+locals {
+  cleaned_input = replace(var.input, "/ +/", " ")   
+  lines = split("\n", trim(local.cleaned_input, "\n"))
+  lines_split = [for line in local.lines: split(" ", line)]
+  left = [for line in local.lines_split: parseint(line[0], 10)]
+  right = [for line in local.lines_split: parseint(line[1], 10)]
+
+  left_sorted = sort(local.left)
+  right_sorted = sort(local.right)
+
+  diffs = [for i in range(length(local.left_sorted)): abs(local.left_sorted[i] - local.right_sorted[i])]
+}
+
+output "part1" {
+  value = sum(local.diffs)
+}
