e65b5a77a7278d6615fe31cc1fa33c9b3cd8877e
Bert Peters <bert@bertptrs.nl>
2024-12-05 08:46:40 +0100

    Implement 2024 day 5

---
+++ 2024/tests/test_day5.py
@@ -0,0 +1,19 @@
+import os
+
+from aoc.days.day5 import DayRunner
+
+def get_data() -> str:
+    sample = os.path.dirname(__file__) + "/samples/05.txt"
+    with open(sample, mode="rt", encoding="utf-8") as f:
+        return f.read()
+
+
+def test_sample_part1() -> None:
+    data = get_data()
+
+    assert DayRunner.part1(data) == 143
+
+def test_sample_part2() -> None:
+    data = get_data()
+
+    assert DayRunner.part2(data) == 123
