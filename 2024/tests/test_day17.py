4a8b9f91099fa03f98645cf5f4c0a54dd0b4ccbf
Bert Peters <bert@bertptrs.nl>
2024-12-17 22:17:12 +0100

    Implement 2024 day 17 part 2

--- 2024/tests/test_day17.py
+++ 2024/tests/test_day17.py
@@ -4,4 +4,8 @@ from . import get_data
 
 
 def test_sample_part1() -> None:
-    assert DayRunner.part1(get_data(17)) == "4,6,3,5,6,3,5,2,1,0"
+    assert DayRunner.part1(get_data(17, 1)) == "4,6,3,5,6,3,5,2,1,0"
+
+
+def test_sample_part2() -> None:
+    assert DayRunner.part2(get_data(17, 2)) == 117440
