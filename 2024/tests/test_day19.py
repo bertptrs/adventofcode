ad5351cfe210e5b37215b9f85e0429e62183b7e4
Bert Peters <bert@bertptrs.nl>
2024-12-19 09:05:55 +0100

    Implement 2024 day 19

---
+++ 2024/tests/test_day19.py
@@ -0,0 +1,11 @@
+from aoc.days.day19 import DayRunner
+
+from . import get_data
+
+
+def test_sample_part1() -> None:
+    assert DayRunner.part1(get_data(19)) == 6
+
+
+def test_sample_part2() -> None:
+    assert DayRunner.part2(get_data(19)) == 16
