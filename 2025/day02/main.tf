0aac02437868b9bba7f3778d073d8416b9150958
Bert Peters <bert@bertptrs.nl>
2025-12-02 09:55:37 +0100

    Attempt at part two, OOM

--- 2025/day02/main.tf
+++ 2025/day02/main.tf
@@ -12,6 +12,18 @@ module "check_range" {
   max = local.min_max[count.index][1]
 }
 
+module "check_range2" {
+  source = "./range2"
+  count  = length(local.min_max)
+
+  min = local.min_max[count.index][0]
+  max = local.min_max[count.index][1]
+}
+
 output "part1" {
   value = sum(module.check_range[*].invalid_sum)
+}
+
+output "part2" {
+  value = sum(module.check_range2[*].invalid_sum)
 }
